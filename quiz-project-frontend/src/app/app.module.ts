8f0af73bb12157fb632c220c30eb910f80091dec
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-03-31 22:03:18 -0500

    Add: Created Sign Up form and link to Backend

--- quiz-project-frontend/src/app/app.module.ts
+++ quiz-project-frontend/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- quiz-project-frontend/src/app/auth/signup/signup.component.ts
+++ quiz-project-frontend/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signupForm = new FormGroup({
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    password: new FormControl(''),
+    password_confirmation: new FormControl(''),
+    username: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(private authService: AuthService, private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.authService.signup(this.signupForm.value).subscribe((res: any) => {
+      if(res.success){
+        this.route.navigate(['/login']);
+      }
+    })
+    console.log(this.signupForm.value);
   }
 
 }
