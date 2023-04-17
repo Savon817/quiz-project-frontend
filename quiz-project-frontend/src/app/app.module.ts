d042c68dd26746ca89acec1e8856bd6832b4c59e
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-04-16 22:25:17 -0500

    Update: Working on submitting QuizForm

--- quiz-project-frontend/src/app/app.module.ts
+++ quiz-project-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,7 +26,8 @@ import { QuizzesComponent } from './quizzes/quizzes.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- quiz-project-frontend/src/app/quiz-detail/quiz-detail.component.ts
+++ quiz-project-frontend/src/app/quiz-detail/quiz-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QuizService } from '../shared/services/quiz.service';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz-detail',
@@ -9,11 +10,20 @@ import { QuizService } from '../shared/services/quiz.service';
 })
 export class QuizDetailComponent implements OnInit {
 
+  quizForm = new FormGroup({
+
+  })
+
+  // quizForm = this.fb.group({
+  //   answer: ['']
+  // })
+
   quiz: any = null;
   creator: any = null;
   quizQuestion: any = [];
+  possible_answers: any;
 
-  constructor(private activatedRoute:ActivatedRoute, private quizService:QuizService) { }
+  constructor(private activatedRoute:ActivatedRoute, private quizService:QuizService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
@@ -23,11 +33,15 @@ export class QuizDetailComponent implements OnInit {
           this.quiz = res.payload.quiz;
           this.creator = res.payload.quiz.user;
           this.quizQuestion = res.payload.quiz.questions;
-          console.log(this.quiz);
-          console.log(this.quizQuestion.possible_answers);
+          console.log(res.payload.quiz.questions[0].possible_answers);
+
         }
       });
     })
+  }
+
+  onSubmit(){
+    console.log(this.quizForm.value);
   }
 
 }
