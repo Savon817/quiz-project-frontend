f040c443b733447a5d1ada892e61e7e3399d547f
Savon Williams <savon817@live.missouristate.edu>
2023-04-17 20:09:08 -0500

    Update: Still working on Quiz Detail Form

--- quiz-project-frontend/src/app/quiz-detail/quiz-detail.component.ts
+++ quiz-project-frontend/src/app/quiz-detail/quiz-detail.component.ts
@@ -10,10 +10,7 @@ import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/
 })
 export class QuizDetailComponent implements OnInit {
 
-  quizForm = new FormGroup({
-    
-  })
-
+  quizForm = new FormGroup({})
   // quizForm = this.fb.group({
   //   answer: ['']
   // })
@@ -21,26 +18,59 @@ export class QuizDetailComponent implements OnInit {
   quiz: any = null;
   creator: any = null;
   quizQuestion: any = [];
-  possible_answers: any;
 
-  constructor(private activatedRoute:ActivatedRoute, private quizService:QuizService, private fb: FormBuilder) { }
+  constructor(private activatedRoute: ActivatedRoute, private quizService: QuizService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
+
+
+      let questions: any = [
+        {
+          question: "This is a test",
+          possible_answers: ["1", "2", "3", "4"]
+        },
+        {
+          question: "This is a test",
+          possible_answers: []
+        },
+        {
+          question: "This is a test",
+          possible_answers: []
+        }
+      ]
+
+      let questionsFormControls: any = {};
+
+      questions.forEach((question, i) => {
+        questionsFormControls[`questions${i}`] = new FormControl('What is this', Validators.required);
+      })
+
+
+      console.log("questionsformcontrols", questionsFormControls)
+      // quizForm = new FormGroup({
+      //   question0: new FormControl('', Validators.required),
+      //   question1: new FormControl('', Validators.required),
+      // })
+
+
       const quizId = params.id;
       this.quizService.fetchQuiz(quizId).subscribe({
-        next: (res:any) => {
+        next: (res: any) => {
           this.quiz = res.payload.quiz;
           this.creator = res.payload.quiz.user;
           this.quizQuestion = res.payload.quiz.questions;
           console.log(res.payload.quiz.questions[0].possible_answers);
-          
+          this.quizQuestion.forEach((question, i) => {
+            this.quizForm[`question${i}`] = new FormControl('', Validators.required)
+          })
+          console.log(this.quizQuestion[0])
         }
       });
     })
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.quizForm.value);
   }
 
